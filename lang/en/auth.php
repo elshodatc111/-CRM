89e630172b58dc0558b066e7bcfcb32e53792856
Elshod Musurmonov <elshodatc1116@gmail.com>
2026-04-08 00:29:05 +0500

    Save

---
+++ lang/en/auth.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'title' => "Login",
+    'welcome' => "Welcome",
+    'phone' => "Phone number",
+    'password' => "Password",
+    'login' => "Sign in",
+
+    'error' => "Invalid phone number or password",
+    'phone_required' => "Please enter your phone number",
+    'phone_format_error' => "Invalid phone number format",
+    'phone_not_found' => "This phone number is not registered or has been deleted",
+    'user_blocked' => "Your account is blocked. Please contact the administrator",
+
+    'password_required' => "Please enter your password",
+    'password_min_error' => "Password must be at least 6 characters",
+    'password_error' => "Password is required",
+
+    'failed' => "Incorrect login or password",
+    'role_access_denied' => "You do not have permission to access this system",
+];
